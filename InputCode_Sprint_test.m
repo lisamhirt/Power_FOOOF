82c1af978ae09deaa9ef2f3d321bcd192ccb6763
lisamhirt <lisa.hirt@cuanschutz.edu>
2024-08-27 11:20:21 -0600

    testing different inputs

---
+++ InputCode_Sprint_test.m
@@ -0,0 +1,14 @@
+%
+
+% getLFPPSD_Sprint(tmpSUB , Hemi , saveLOC)
+
+getLFPPSD_Sprint_test('CLASE018' , 'Y:\LossAversion\LH_tempSave\CLASE018\SPOOOF_test\SPRiNT_out')
+
+
+%% 
+
+% for my decision screen it is less than 1 second so I will have to loop 
+% through the vector of that epoch. 
+
+% Take sampling rate (500) and divide that by the number of samples in the
+% vector (eg: 222). 
