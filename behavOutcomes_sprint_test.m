bb61fa683a9e707943747d3371da1e7abe074bee
Lisa Hirt <lisa.hirt@cuanschutz.edu>
2024-09-03 10:48:19 -0600

    testing sprint output

--- behavOutcomes_sprint_test.m
+++ behavOutcomes_sprint_test.m
@@ -1,4 +1,8 @@
 % do amp next 
+% Load SPRiNT data
+cd('X:\LossAversion\Patient folders\CLASE018\NeuroPhys_Processed\SPRINT');
+load('CLASE018_L_AMY.mat'); % loads as allSprintOut
+
 % Behavior data
 cd('X:\LossAversion\Patient folders\CLASE018\Behavioral-data')
 eventTab = load("clase_behavior_CLASE018_738812.7161.mat");
@@ -30,83 +34,103 @@ outcomeGain = eventTab.subjdata.cs.outcome > 0 & ~checkIndex;
 
 gamblesLA = all(gainLOSS_trials & gamble_trials, 2);
 gambleOUT_gain = all(gamblesLA & outcomeGain, 2);
+gambleOUT_gain = double(gambleOUT_gain);
+
+altLA = all(gainLOSS_trials & alternative_trials,2);
+altOUT = all(altLA & outcomeNeutral, 2);
+altOUT = double(altOUT);
+altOUT(altOUT == 1) = 3;
+
+gambleOUT_gain(gambleOUT_gain == 0) = altOUT(gambleOUT_gain == 0); % alternative is 3, gamble gain is 1, gamble loss is 0
+
+
 % gambleOUT_loss = all(gamblesLA & outcomeLoss, 2);
 
 % altTrial = rand(135, 1)<0.5;
 
 lossPeaks = [];
 gainPeaks = [];
+altPeaks = [];
 
 for i = 1:length(gambleOUT_gain)
     if gambleOUT_gain(i) == 1
         tempTrial = allSprintOut{1,i}{1,1}.channel.peaks;
         tempTrialTAB = struct2table(tempTrial);
-        tempFreq = tempTrialTAB.center_frequency;
+        % tempFreq = tempTrialTAB.center_frequency;
+        tempFreq = tempTrialTAB.amplitude;
 
-        deltaIDX = find(tempFreq >= 1 & tempFreq <= 4);
-        deltaAVG = mean(tempFreq(deltaIDX));
+        % deltaIDX = find(tempFreq >= 1 & tempFreq <= 4);
+        % deltaAVG = mean(tempFreq(deltaIDX));
+        % 
+        % thetaIDX = find(tempFreq > 4.01 & tempFreq <= 8);
+        % thetaAVG = mean(tempFreq(thetaIDX));
+        % 
+        % alphaIDX = find(tempFreq > 8.01 & tempFreq <= 12);
+        % alphaAVG = mean(tempFreq(alphaIDX));
+        % 
+        % betaIDX = find(tempFreq > 12.01 & tempFreq <= 30);
+        % betaAVG = mean(tempFreq(betaIDX));
+        % 
+        % LowGammaIDX = find(tempFreq >= 30.01 & tempFreq <= 50);
+        % LowGammaAvg = mean(tempFreq(LowGammaIDX));
+        % 
+        % HighGammaIDX = find(tempFreq >= 50);
+        % HighGammaAvg = mean(HighGammaIDX);
 
-        thetaIDX = find(tempFreq > 4.01 & tempFreq <= 8);
-        thetaAVG = mean(tempFreq(thetaIDX));
+        % lossPeaks = [lossPeaks; deltaAVG; thetaAVG;alphaAVG;betaAVG;LowGammaAvg;HighGammaAvg];
 
-        alphaIDX = find(tempFreq > 8.01 & tempFreq <= 12);
-        alphaAVG = mean(tempFreq(alphaIDX));
-
-        betaIDX = find(tempFreq > 12.01 & tempFreq <= 30);
-        betaAVG = mean(tempFreq(betaIDX));
-
-        LowGammaIDX = find(tempFreq >= 30.01 & tempFreq <= 50);
-        LowGammaAvg = mean(tempFreq(LowGammaIDX));
-
-        HighGammaIDX = find(tempFreq >= 50);
-        HighGammaAvg = mean(HighGammaIDX);
-
-        lossPeaks = [lossPeaks; deltaAVG; thetaAVG;alphaAVG;betaAVG;LowGammaAvg;HighGammaAvg];
-
-        % lossPeaks = [lossPeaks; tempFreq];
+        lossPeaks = [lossPeaks; tempFreq];
 
     else
         tempTrial = allSprintOut{1,i}{1,1}.channel.peaks;
         tempTrialTAB = struct2table(tempTrial);
-        tempFreq = tempTrialTAB.center_frequency;
+        % tempFreq = tempTrialTAB.center_frequency;
+        tempFreq = tempTrialTAB.amplitude;
 
-          deltaIDX = find(tempFreq >= 1 & tempFreq <= 4);
-        deltaAVG = mean(tempFreq(deltaIDX));
 
-        thetaIDX = find(tempFreq > 4.01 & tempFreq <= 8);
-        thetaAVG = mean(tempFreq(thetaIDX));
+        % deltaIDX = find(tempFreq >= 1 & tempFreq <= 4);
+        % deltaAVG = mean(tempFreq(deltaIDX));
+        % 
+        % thetaIDX = find(tempFreq > 4.01 & tempFreq <= 8);
+        % thetaAVG = mean(tempFreq(thetaIDX));
+        % 
+        % alphaIDX = find(tempFreq > 8.01 & tempFreq <= 12);
+        % alphaAVG = mean(tempFreq(alphaIDX));
+        % 
+        % betaIDX = find(tempFreq > 12.01 & tempFreq <= 30);
+        % betaAVG = mean(tempFreq(betaIDX));
+        % 
+        % LowGammaIDX = find(tempFreq >= 30.01 & tempFreq <= 50);
+        % LowGammaAvg = mean(tempFreq(LowGammaIDX));
+        % 
+        % HighGammaIDX = find(tempFreq >= 50);
+        % HighGammaAvg = mean(HighGammaIDX);
 
-        alphaIDX = find(tempFreq > 8.01 & tempFreq <= 12);
-        alphaAVG = mean(tempFreq(alphaIDX));
-
-        betaIDX = find(tempFreq > 12.01 & tempFreq <= 30);
-        betaAVG = mean(tempFreq(betaIDX));
-
-        LowGammaIDX = find(tempFreq >= 30.01 & tempFreq <= 50);
-        LowGammaAvg = mean(tempFreq(LowGammaIDX));
-
-        HighGammaIDX = find(tempFreq >= 50);
-        HighGammaAvg = mean(HighGammaIDX);
-
-        gainPeaks =  [gainPeaks; deltaAVG; thetaAVG;alphaAVG;betaAVG;LowGammaAvg;HighGammaAvg];
-        % gainPeaks = [gainPeaks; tempFreq];
+        % gainPeaks =  [gainPeaks; deltaAVG; thetaAVG;alphaAVG;betaAVG;LowGammaAvg;HighGammaAvg];
+        gainPeaks = [gainPeaks; tempFreq];
 
     end % if else 
 
 end % for / i 
 
-sw1 = swarmchart(zeros(length(lossPeaks),1),lossPeaks,20,"magenta", "filled")
+normLAGain = normalize(gainPeaks,'range');
+normLALoss =  normalize(lossPeaks,'range');
+
+logLAGain = log(gainPeaks);
+logLALoss = log(lossPeaks);
+
+sw1 = swarmchart(zeros(length(logLALoss),1),logLALoss,20,"magenta", "filled")
 sw1.XJitter = "rand";
 sw1.XJitterWidth = 0.2;
 
 hold on 
 
-sw2 = swarmchart(ones(length(gainPeaks),1),gainPeaks,20,"blue", "filled")
+sw2 = swarmchart(ones(length(logLAGain),1),logLAGain,20,"blue", "filled")
 sw2.XJitter = "rand";
 sw2.XJitterWidth = 0.2;
 
 namedian(lossPeaks)
 
-[a,b,c] = kstest2(lossPeaks, gainPeaks);
+[a,b,c] = kstest2(logLALoss, logLAGain);
 
 ranksum(gainPeaks, lossPeaks);
